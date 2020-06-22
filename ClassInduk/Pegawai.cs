02e0971b0d5da568f2603648e6a7b498f9da4b7e
Muhammad Fahmi Attaqwa <Muhammad.attaqwa@students.amikom.ac.id>
2020-06-22 21:15:14 +0700

    Class Induk

---
+++ ClassInduk/Pegawai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.ClassInduk
+{
+    public abstract class Pegawai
+    {
+        public int NIK { get; set; }
+        public string NAMA { get; set; }
+        public abstract double GAJI();
+    }
+}
