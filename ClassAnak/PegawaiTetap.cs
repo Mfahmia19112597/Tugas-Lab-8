c9760c04b82ca3838635cc5e685d93c1299b30eb
Muhammad Fahmi Attaqwa <Muhammad.attaqwa@students.amikom.ac.id>
2020-06-22 21:13:52 +0700

    Class Anak

---
+++ ClassAnak/PegawaiTetap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.ClassAnak
+{
+    public class PegawaiTetap : ClassInduk.Pegawai
+    {
+        public double gajibulanan { get; set; }
+        public override double GAJI()
+        {
+            return gajibulanan;
+        }
+    }
+}
---
+++ ClassAnak/Sales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.ClassAnak
+{
+    public class Sales : ClassInduk.Pegawai
+    {
+        public double jumlahpenjualan { get; set; }
+        public double komisi { get; set; }
+        public override double GAJI()
+        {
+            return komisi * jumlahpenjualan;
+        }
+    }
+}
